309b126b0b80c6fd49af788d9c96a94f5cabd052
mikaelnystroms <mikaelnystromm@gmail.com>
2023-02-07 18:30:59 +0100

    live demo links on projects

--- pages/mywork/index.vue
+++ pages/mywork/index.vue
@@ -13,7 +13,7 @@ const { data: projects } = await useFetch('https://notion-api.splitbee.io/v1/tab
                     :excerpt="project.Text"
                     :image="project['Files & media'][0].url"
                     :tag="project.Tag"
-                    :link="project.URL"
+                    :link="project.urllink"
                 />
             </div>
         </section>
